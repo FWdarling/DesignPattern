42c89c9c35da0e57763f93e8477de729b0bb3ae7
Max Leung <maxleung99@qq.com>
2020-11-13 15:31:13 +0800

    remove redundant modification

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,3 @@
-import adapter.AdapterDemo;
-import callback.CallbackDemo;
-import converter.ConverterDemo;
-import template.TemplateDemo;
-
 public class Main {
     public static void main(String[] args){
         System.out.println("hello, world!\n");
