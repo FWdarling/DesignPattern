7a260796b292bc204edf85c6cfe08a5f6a808058
CindyCindy424 <64152119+CindyCindy424@users.noreply.github.com>
2020-11-16 14:43:45 +0800

    first-version (#4)
    * first-version
    
    * Prototype/Interpreter/Visitor/DoubleCheckedLocking//v1.2
    
    * modify package names according to naming-regulations
    
    * modify names of packages -2
    
    * Delete OlympicsYard.java
    
    * Delete Const.java
    
    * Delete AddExpression.java
    
    * Delete Expression.java
    
    * Delete MatchException.java
    
    * Delete MatchExpression.java
    
    * Delete MultipleExpression.java
    
    * Delete TerminalExpression.java
    
    * Delete Athlete.java
    
    * Delete Certificate.java
    
    * Delete Prize.java
    
    * Delete Area.java
    
    * Delete Circle.java
    
    * Delete Element.java
    
    * Delete Perimeter.java
    
    * Delete Square.java
    
    * Delete Triangle.java
    
    * Delete Visitor.java
    
    * Delete DoubleCheckedLockingTest.java
    
    * Delete InterpreterTest.java
    
    * Delete PrototypeTest.java
    
    * Delete VisitorTest.java
    
    * modify package-naming -3
    
    * modify package-naming -3

---
+++ src/test/java/prototype/PrototypeTest.java
@@ -0,0 +1,39 @@
+package prototype;
+
+import prototype.entity.Athlete;
+import org.junit.Test;
+
+/**
+ * @Author: CINDY DENG
+ * @Date: 2020/11/12,22:54
+ * @Version: 1.0
+ */
+public class PrototypeTest {
+    @Test
+    public void test() throws CloneNotSupportedException{
+        System.out.println("====Prototype===");
+        Athlete athlete1 = new Athlete("ALEX", "highJump");
+        Athlete athlete2 = new Athlete("Cathy","highJump");
+        Athlete athlete3 = new Athlete("Tim","highJump");
+        Athlete athlete4 = new Athlete("Jerry","highJump");
+
+        Certificate tmpCertificate = new Certificate(athlete1.getName(),athlete1.getSportsType(), Prize.FPRSTPRIZE);
+        tmpCertificate.printCertificate();
+
+        Certificate tmpCertificate2 = (Certificate)tmpCertificate.clone();
+        tmpCertificate2.setName(athlete2.getName());
+        tmpCertificate2.setPrizeLevel(Prize.SECONDPRIZE);
+
+        Certificate tmpCertificate3 = (Certificate)tmpCertificate.clone();
+        tmpCertificate3.setName(athlete3.getName());
+        tmpCertificate3.setPrizeLevel(Prize.SECONDPRIZE);
+
+        Certificate tmpCertificate4 = (Certificate)tmpCertificate.clone();
+        tmpCertificate4.setName(athlete4.getName());
+        tmpCertificate4.setPrizeLevel(Prize.THIRDPRIZE);
+
+        tmpCertificate2.printCertificate();
+        tmpCertificate3.printCertificate();
+        tmpCertificate4.printCertificate();
+    }
+}
