3f64a67acd6ab5d9ea33d5dbfd0e6265da8dadf8
Nagisa_Cheese <kotori@tongji.edu.cn>
2020-11-16 23:19:50 +0800

    Fix specification return type

--- src/test/java/specification/SpecificationTest.java
+++ src/test/java/specification/SpecificationTest.java
@@ -8,10 +8,10 @@ import specification.entity.*;
 
 public class SpecificationTest {
     
-    private void boxingSelector(AbstractAthlete athlete) {
+    private boolean boxingSelector(AbstractAthlete athlete) {
         final var foodTypeSelector = new FoodTypeSelector(FoodType.CARNIVORE);
         final var massSelector = new MassGreaterThanSelector(200);
-        foodTypeSelector.and(massSelector).test(athlete);
+        return foodTypeSelector.and(massSelector).test(athlete);
     }
 
     @Test
