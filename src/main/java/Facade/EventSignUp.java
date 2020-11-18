7fc51cc14f88d4bd51e260b8bf2623d50786726f
cjsmt <45128328+cjsmt@users.noreply.github.com>
2020-11-18 12:42:06 +0800

    Facade/Memento/DAO (#12)
    * Facade/Memento/DAO
    
    * facade/memento_v2.0

---
+++ src/main/java/Facade/EventSignUp.java
@@ -0,0 +1,33 @@
+package facade;
+
+import facade.entity.Event;
+
+/**
+ * description: 子系统类，用于定义产生几种不同的子系统
+ * data: 11/18/20 01：49 AM
+ * author: cjsmt
+ */
+public class EventSignUp {
+    private Event event;
+
+    public EventSignUp(Event event) {
+        this.event = event;
+    }
+
+    public void signUp(String name) {
+        String text;
+        switch (event){
+            case RUN:
+                text = "running"; break;
+            case FLY:
+                text = "flying"; break;
+            case SWIM:
+                text = "swimming";  break;
+            case PING_PONG:
+                text = "table tennis";  break;
+            default:
+                text = event.toString().toLowerCase();  break;
+        }
+        System.out.println("Dear " + name + ", you have successfully signed up for the " + text + " competition.");
+    }
+}
---
+++ src/test/java/facade/FacadeTest.java
@@ -0,0 +1,15 @@
+package facade;
+
+/**
+ * description: 外观模式测试类
+ * data: 11/17/20 20：09 PM
+ * author: cjsmt
+ */
+public class FacadeTest {
+    public void facadeTest(){
+
+        System.out.println("========  测试外观模式开始  ========");
+        FacadeDemo.facadeTest();
+        System.out.println("========  测试外观模式结束  ========");
+    }
+}
