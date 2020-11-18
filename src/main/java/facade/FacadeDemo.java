33dfe5f17d031a334400358af2533747694752e7
cjsmt <1910809353@qq.com>
2020-11-18 19:27:25 +0800

    facade/memento_v2.0

---
+++ src/main/java/facade/FacadeDemo.java
@@ -0,0 +1,24 @@
+package facade;
+
+import facade.entity.*;
+
+/**
+ * description: 外观模式demo调用类
+ * data: 11/13/20 01:30 AM
+ * author: cjsmt
+ */
+public class FacadeDemo {
+
+    public static void facadeTest(){
+        Animal bird = new Bird("zhizhi");
+        Animal fish = new Fish("paopao");
+        Animal rabbit = new Rabbit("tiaotiao");
+
+        RegisterForm registerForm1 = new RegisterForm(bird);   registerForm1.signUp();
+        RegisterForm registerForm2 = new RegisterForm(fish);   registerForm2.signUp();
+        RegisterForm registerForm3 = new RegisterForm(rabbit); registerForm3.signUp();
+
+        rabbit.setEventToSignUp(Event.RUN);
+        RegisterForm registerForm4 = new RegisterForm(rabbit); registerForm4.signUp();
+    }
+}
---
+++ src/main/java/memento/MementoDemo.java
@@ -0,0 +1,32 @@
+package memento;
+
+import memento.entity.Athlete;
+import memento.entity.Event;
+import memento.entity.Species;
+
+/**
+ * description: 备忘录模式(memento)调用类
+ * data: 11/13/20 17:25 PM
+ * author: cjsmt
+ */
+public class MementoDemo {
+
+    public static void mementoTest(){
+        EventEntryForm eventEntryForm = new EventEntryForm(Event.RUN);
+        MemoManager memoManager = new MemoManager();
+        Athlete athlete1 = new Athlete("niao", Species.Bird);
+        Athlete athlete2 = new Athlete("chongzi", Species.Insect);
+        Athlete athlete3 = new Athlete("qiuqiu", Species.Cat);
+
+
+        memoManager.setEntryFormMemo(eventEntryForm.createMemo());  //创建备忘录
+        eventEntryForm.processRegistration(memoManager.getEntryFormMemo(), athlete1);  //处理报名
+
+        memoManager.setEntryFormMemo(eventEntryForm.createMemo());  //创建备忘录
+        eventEntryForm.processRegistration(memoManager.getEntryFormMemo(), athlete2);  //处理报名
+
+        memoManager.setEntryFormMemo(eventEntryForm.createMemo());  //创建备忘录
+        eventEntryForm.processRegistration(memoManager.getEntryFormMemo(), athlete3);  //处理报名
+
+    }
+}
