c8f593f81bae816725f46f9a695bf71b0c6d9b47
CindyCindy424 <64152119+CindyCindy424@users.noreply.github.com>
2020-11-22 01:26:51 +0800

    modify prototype pattern -- add a deep copy version (#22)

---
+++ src/main/java/prototypedeepcopy/AwardLevel.java
@@ -0,0 +1,10 @@
+package prototypedeepcopy;
+
+/**
+ * Author: CINDY DENG 1850477
+ * Date: 2020/11/21,12:35
+ * Version: 1.0
+ */
+public enum AwardLevel {
+    FPRSTPRIZE,SECONDPRIZE,THIRDPRIZE
+}
