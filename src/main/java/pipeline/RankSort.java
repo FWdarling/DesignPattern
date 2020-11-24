cc901bf01cba78bf58ba0d54cb8e4b9843b12b53
cjsmt <1910809353@qq.com>
2020-11-25 02:05:35 +0800

    为部分方法添加了注释

--- src/main/java/pipeline/RankSort.java
+++ src/main/java/pipeline/RankSort.java
@@ -10,10 +10,20 @@ package pipeline;
 public class RankSort<I, O> {
     private final Sort<I, O> currentSort;
 
+    /**
+     * description:构造函数，初始化管道类
+     * @param currentSort
+     */
     RankSort(Sort<I, O> currentSort){
         this.currentSort = currentSort;
     }
 
+    /**
+     * description:添加后续阶段类，用点方法调用
+     * @param newSort
+     * @param <K>
+     * @return
+     */
     <K> RankSort<I, K> addSort(Sort<O, K> newSort) {
         return new RankSort<>(input -> newSort.process(currentSort.process(input)));
     }
