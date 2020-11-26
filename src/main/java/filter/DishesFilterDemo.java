05648fe4f274006165e459e255e4671b72578f32
FWdarling <1853204@tongji.edu.cn>
2020-11-26 13:01:56 +0800

    * Format code
    * add annotations for every function
    
    * add tips output in demo code`

--- src/main/java/filter/DishesFilterDemo.java
+++ src/main/java/filter/DishesFilterDemo.java
@@ -11,8 +11,18 @@ import java.util.ArrayList;
  * author: fourwood
  */
 public class DishesFilterDemo {
+    /*
+     * description: 过滤器模式演示代码
+     * date: 11/26/20 12:33 PM 
+     * author: fourwood 
+     * 
+     * @param 
+     * @return void
+     */ 
     public static void dishesFilterTest() {
         ArrayList<Dishes> dishesArrayList = new ArrayList<Dishes>();
+        System.out.println("                                        [ Dishes : Dishes() :  构造函数， 创建菜品]");
+        System.out.println("开始创建菜单！");
         Dishes spicyDishes1 = new Dishes("辣子鸡", Taste.spicy);
         dishesArrayList.add(spicyDishes1);
         Dishes spicyDishes2 = new Dishes("麻婆豆腐", Taste.spicy);
@@ -30,12 +40,23 @@ public class DishesFilterDemo {
         Dishes sweetDishes2 = new Dishes("拔丝地瓜", Taste.sweet);
         dishesArrayList.add(sweetDishes2);
 
+        System.out.println("                                        [ SpicyDishesFilter : filter() :  重载父类的抽象方法， 筛选辣的菜品]");
+        System.out.println("开始生成辣的菜单！");
         SpicyDishesFilter spicyDishesFilter = new SpicyDishesFilter();
         ArrayList<Dishes> spicyDishes = spicyDishesFilter.filter(dishesArrayList);
+
+        System.out.println("                                        [ SourDishesFilter : filter() :  重载父类的抽象方法， 筛选酸的菜品]");
+        System.out.println("开始生成酸的菜单！");
         SourDishesFilter sourDishesFilter = new SourDishesFilter();
         ArrayList<Dishes> sourDishes = sourDishesFilter.filter(dishesArrayList);
+
+        System.out.println("                                        [ SaltyDishesFilter : filter() :  重载父类的抽象方法， 筛选咸的菜品]");
+        System.out.println("开始生成咸的菜单！");
         SaltyDishesFilter saltyDishesFilter = new SaltyDishesFilter();
         ArrayList<Dishes> saltyDishes = saltyDishesFilter.filter(dishesArrayList);
+
+        System.out.println("                                        [ SweetDishesFilter : filter() :  重载父类的抽象方法， 筛选甜的菜品]");
+        System.out.println("开始生成甜的菜单！");
         SweetDishesFilter sweetDishesFilter = new SweetDishesFilter();
         ArrayList<Dishes> sweetDishes = sweetDishesFilter.filter(dishesArrayList);
 
--- src/main/java/flyweight/FlyWeightDemo.java
+++ src/main/java/flyweight/FlyWeightDemo.java
@@ -10,19 +10,36 @@ import entity.SportType;
  */
 public class FlyWeightDemo {
 
+    /*
+     * description: 享元模式演示代码
+     * date: 11/26/20 12:53 PM
+     * author: fourwood 
+     * 
+     * @param 
+     * @return void
+     */ 
     public static void flyWeightTest(){
+        System.out.println("                                        [ Game : Game() : 构造函数， 创建比赛 ]");
         Game runGame1 = new Game(SportType.run);
         Game runGame2 = new Game(SportType.run);
         Game runGame3 = new Game(SportType.run);
 
+        System.out.println("                                        [ Game : assignReferee() : 尝试为比赛分配裁判（使用了享元模式） ]");
         runGame1.assignReferee();
+        System.out.println("                                        [ Game : getReferee() : 获取为比赛分配好的裁判 ]");
         System.out.println("跑步比赛1的裁判是： " + runGame1.getReferee().getName());
+        System.out.println("                                        [ Game : assignReferee() : 尝试为比赛分配裁判（使用了享元模式） ]");
         runGame2.assignReferee();
+        System.out.println("                                        [ Game : getReferee() : 获取为比赛分配好的裁判 ]");
         System.out.println("跑步比赛2的裁判是： " + runGame2.getReferee().getName());
 
         //让1号裁判空闲，再为跑步比赛3分配裁判，测试享元模式
+        System.out.println("                                        [ Game : over() : 终止某一场比赛， 修改该比赛的裁判的状态为free ]");
         runGame1.over();
+        System.out.println("跑步比赛1结束!");
+        System.out.println("                                        [ Game : assignReferee() : 尝试为比赛分配裁判（使用了享元模式） ]");
         runGame3.assignReferee();
+        System.out.println("                                        [ Game : getReferee() : 获取为比赛分配好的裁判 ]");
         System.out.println("跑步比赛3的裁判是： " + runGame3.getReferee().getName());
     }
 }
