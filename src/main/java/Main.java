c3e4f49e6ddc92e3d7380e2dec1553ee49592aed
FWdarling <1853204@tongji.edu.cn>
2020-11-26 14:43:29 +0800

    add WorkerThread pattern demo code into main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -68,6 +68,8 @@ import template.TemplateDemo;
 import transferobject.PersonalInformation;
 import transferobject.QuerySystem;
 import visitor.*;
+import workerthread.CheckPoint;
+import workerthread.RunnerThread;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -693,5 +695,35 @@ public class Main {
         }
         System.out.println("------------------------ END ------------------------");
         System.out.println("");
+
+
+        System.out.println("---------------- [Pattern] WorkerThread ----------------");
+
+        final CheckPoint checkPoint = new CheckPoint(99);
+        checkPoint.startVolunteer();
+
+        RunnerThread t1 = new RunnerThread("Nemo", checkPoint);
+        RunnerThread t2 = new RunnerThread("Four", checkPoint);
+        RunnerThread t3 = new RunnerThread("Wood", checkPoint);
+
+        // 开始并等待所有 Runner 线程结束
+        try {
+            t1.start();
+            t2.start();
+            t3.start();
+
+            t1.join();
+            t2.join();
+            t3.join();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        finally {
+            // 手动结束所有的 Volunteer 线程
+            checkPoint.stopVolunteer();
+            System.out.println("------------------------ END ------------------------");
+            System.out.println("");
+        }
     }
 }
