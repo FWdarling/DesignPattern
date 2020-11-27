262aed64693ee1cbf36f93244a3d7dff5a162b4c
MatrixTZuZT <823642840@qq.com>
2020-11-27 21:58:25 +0800

    添加了注释

--- src/main/java/decorator/InformationSenderForTennis.java
+++ src/main/java/decorator/InformationSenderForTennis.java
@@ -2,6 +2,9 @@ package decorator;
 
 public class InformationSenderForTennis extends InformationSender {
     @Override
+    /**
+     * 发送信息
+     */
     public void sendInformation(String message) {
         //获取相应的wrapper
         String wrapper = getWrapper("Tennisnews", message.length(), "*");
