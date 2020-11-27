f78b494237c87438044aec5fd08870871d0724a4
EternalAddiCtion <546284705@qq.com>
2020-11-27 13:13:05 +0800

    add print information for the demo (#43)
    Co-authored-by: Max Leung <maxleung99@qq.com>

--- src/main/java/lazyloading/LazyLoadingDemo.java
+++ src/main/java/lazyloading/LazyLoadingDemo.java
@@ -12,6 +12,8 @@ public class LazyLoadingDemo {
     public static void lazyLoadingTest(){
         System.out.println("动物运动会官方工作人员信息公布：\n");
         StuffInfoListProxy stuffInfoListProxy=new StuffInfoListProxy();
+        System.out.println("                                        " +
+                "[ StuffInfoListProxy : getStuffList() : 调用代理获得工作人员信息列表 ]");
         ArrayList<Stuff> stuffList=stuffInfoListProxy.getStuffList();
         for (Stuff stuff:stuffList){
             System.out.println(stuff);
