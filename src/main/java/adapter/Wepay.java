07c906cd37cc25e5977cf1b2ed7c1580c1a79f0e
Max Leung <maxleung99@qq.com>
2020-11-27 18:56:23 +0800

    add annotations

--- src/main/java/adapter/Wepay.java
+++ src/main/java/adapter/Wepay.java
@@ -10,10 +10,28 @@ package adapter;
 public class Wepay {
     private double transactionLimits=5000;
 
+    /**
+     Get the limits of a single transaction which is platform-dependent.
+
+     Args:
+
+     Return:
+        double
+     **/
     public double getTransactionLimits(){
         return transactionLimits;
     }
 
+    /**
+     The login interface provided by the simulated Wepay.The function is to get the user's input and confirm the identity.
+
+     Args:
+     String weID:Account information
+     String pwd:password information
+
+     Return:
+     boolean:whether the login is successful.
+     **/
     public boolean loginWechat(String weId,String pwd){
         if(pwd.equals("1234")){
             return true;
@@ -22,6 +40,16 @@ public class Wepay {
             return false;
         }
     }
+
+    /**
+     The interface to pay the bill provided by Wepay.
+
+     Args:
+     double amount: The amount of the bill
+
+     Return:
+     boolean:Whether the payment is successful.
+     **/
     public boolean payment(double amount){
         if(amount>5000){
             return false;
