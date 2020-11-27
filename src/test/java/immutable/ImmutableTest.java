015461d99c73df6d876cc7839df4d64dcbc9d96a
cjsmt <1910809353@qq.com>
2020-11-27 13:41:31 +0800

    添加了immutable的test类的注解

--- src/test/java/immutable/ImmutableTest.java
+++ src/test/java/immutable/ImmutableTest.java
@@ -1,11 +1,15 @@
 package immutable;
 
+import org.junit.Test;
+
 /**
  * description: Immutable模式测试类
  * date: 11/24/20 02:03 AM
  * author: cjsmt
  */
 public class ImmutableTest {
+
+    @Test
     public void immutableTest(){
         System.out.println("---------------- [Pattern] Immutable ----------------");
         try {
